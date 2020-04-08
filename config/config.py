3b9fdab3adda6bc1831bbfb4320e7b0dd9eaa1c1
Theofanis Petkos <tpetkos@veturilo.io>
2020-04-09 00:14:07 +0300

    Update configuration file

--- config/config.py
+++ config/config.py
@@ -22,11 +22,20 @@ class Config(object):
     # Tests Configuration
     TESTS_URL = 'https://127.0.0.1:5000'
 
-    # Mail Configuration
-    MAIL_SERVER = 'smtp.gmail.com'
-    MAIL_PORT = 465
-    MAIL_USE_TLS = False
-    MAIL_USE_SSL = True
-    MAIL_USERNAME = os.getenv('MAIL_USERNAME')
-    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
-    PRODUCTION_SERVER_IP = os.getenv('PRODUCTION_SERVER_IP')
+    # Constants - Fixed vaules for examples:
+    ICON_SIDE_SIZE = 8
+    ICON_MAX_LENGTH = ICON_SIDE_SIZE ** 2
+    NETWORK_MAX_DEPTH = 6
+
+    # Network settings
+    BIAS = -1.8
+    WEIGHT_LOW = 0.9
+    WEIGHT_HIGH = 1.8
+
+    # Basic settings of learning
+    ERROR_THRESHOLD = 5.0
+    INFLATION = 1.25
+    DEFLATION = 0.8
+    MIN_DESCENT_RATE = 0.00001
+    MAX_DESCENT_RATE = 50.0
+    MAX_ITERATIONS = 5000
