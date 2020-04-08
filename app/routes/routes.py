09c9364457c80a1786314da7d888a0b16c51d9f9
Theofanis Petkos <tpetkos@veturilo.io>
2020-04-09 00:13:57 +0300

    Remove constraints from routes

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -1,23 +1,7 @@
-from app.models.t_examples import TExamples
-from app.models.t_data_sets import TDataSets
-
-# Constants - Fixed vaules for examples:
-ICON_SIDE_SIZE = 8
-ICON_MAX_LENGTH = SIDE_SIZE ** 2
-NETWORK_MAX_DEPTH = 6
-
-# Network settings
-BIAS = -1.8
-WEIGHT_LOW = 0.9
-WEIGHT_HIGH = 1.8
-
-# Basic settings of learning
-ERROR_THRESHOLD = 5.0
-INFLATION = 1.25
-DEFLATION = 0.8
-MIN_DESCENT_RATE = 0.00001
-MAX_DESCENT_RATE = 50.0
-MAX_ITERATIONS = 5000
+from app.models.t_examples import TExample
+from app.models.t_data_sets import TDataSet
+from app.tasks.random_generators import integer_generator, float_generator
+from app.tasks.sigmoid import sigmoid, sigmoid_derivative
 
 # Public Variables
 t_values_array = []
